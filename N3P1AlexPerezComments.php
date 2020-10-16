bb79ec060f712880818d26f8b34d2cccbf1688dc
daw2-Perez21 <71894698+daw2-Perez21@users.noreply.github.com>
2020-10-16 17:14:44 +0200

    Create N3P1AlexPerezComments.php

---
+++ N3P1AlexPerezComments.php
@@ -0,0 +1,34 @@
+<?php
+echo "Create a list of the 10 most important concepts about what you’ve learned in this  document";
+echo "<br>";
+echo "1-Use Style";
+echo "<br>";
+echo "2-Use mailto";
+echo "<br>";
+echo "3-Css in PHP";
+echo "<br>";
+echo "4-Counter";
+echo "<br>";
+echo "5-Use date_diff";
+echo "<br>";
+echo "6-Use cookie";
+echo "<br>";
+echo "7-Use time()";
+echo "<br>";
+echo "8-Use text-align:";
+echo "<br>";
+echo "9-Use strtotime()";
+echo "<br>";
+echo "10-Use isset()";
+echo "<br>";
+echo "Qualifies the document:7";
+echo "<br>";
+echo "Teacher's explanation:6";
+echo "<br>";
+echo "My mark:7";
+echo "<br>";
+echo "Explain an improvement";
+echo "<br>";
+echo "Hay algunos ejercicios que en casa no se entendia lo que pedias podrias poner una foto del resultado que te gustaria ver 
+para hacerse una idea si esta bien o no";
+?>
